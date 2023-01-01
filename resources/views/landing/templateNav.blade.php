dcb723b4c96951c546c10e3dc1915e7f655f1fcd
winataa <mwinata03@gmail.com>
2023-01-01 22:12:22 +0800

    ngilangin rate sm nambahin navbar tapi gamau navbarnya

--- resources/views/landing/templateNav.blade.php
+++ resources/views/landing/templateNav.blade.php
@@ -1,99 +1,36 @@
-<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-      <a class="navbar-brand brand-logo mr-5" href="index.php"><img src="public/admin/images/logo.svg" class="mr-2" alt="logo"/></a>
-      <a class="navbar-brand brand-logo-mini" href="index.php"><img src="public/admin/images/logo-mini.svg" alt="logo"/></a>
-    </div>
-    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-      <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-        <span class="icon-menu"></span>
-      </button>
-      <ul class="navbar-nav mr-lg-2">
-        <li class="nav-item nav-search d-none d-lg-block">
-          <div class="input-group">
-            <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
-              <span class="input-group-text" id="search">
-                <i class="icon-search"></i>
-              </span>
-            </div>
-            <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
+<div style="width:100%;" data-aos="fade-down">
+  <nav class="navbar navbar-expand-lg navbar-light navigasi" style="width: 100%;">
+        <div class="container cnav" style="width: 100%;">
+            <img src="img/logo.png" alt="Bootstrap" width="" height="76">
+            <a class="navbar-brand nav-b ms-4" style="color: #0455c0" href="#">Tadika Laundry</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav" >
+              <li class="nav-item nav-i">
+                <a class="nav-link  nav-l active" aria-current="page" href="/register" style="color: #0455c0">Home</a>
+              </li>
+              <li class="nav-item mx-0.1">
+                <a class="nav-link nav-l " href="#varian" style="color: #0455c0">Service</a>
+              </li>
+              <li class="nav-item mx-0.1">
+                <a class="nav-link nav-l " href="/pricing" style="color: #0455c0">Pricing</a>
+              </li>
+              <li class="nav-item mx-0.1">
+                <a class="nav-link nav-l " href="/faq" style="color: #0455c0">FAQ</a>
+              </li>
+              <li class="nav-item mx-0.1">
+                <a class="nav-link nav-l " href="/contact-create" style="color: #0455c0">Contact</a>
+              </li>
+              <li class="nav-item mx-0.1">
+                <a class="btn btn-primary tombol px-4" style="margin-left: 50px" href="/register">REGIS</a>
+              </li>
+            </ul>
           </div>
-        </li>
-      </ul>
-      <ul class="navbar-nav navbar-nav-right">
-        <li class="nav-item dropdown">
-          <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-            <i class="icon-bell mx-0"></i>
-            <span class="count"></span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-            <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
-            <a class="dropdown-item preview-item">
-              <div class="preview-thumbnail">
-                <div class="preview-icon bg-success">
-                  <i class="ti-info-alt mx-0"></i>
-                </div>
-              </div>
-              <div class="preview-item-content">
-                <h6 class="preview-subject font-weight-normal">Application Error</h6>
-                <p class="font-weight-light small-text mb-0 text-muted">
-                  Just now
-                </p>
-              </div>
-            </a>
-            <a class="dropdown-item preview-item">
-              <div class="preview-thumbnail">
-                <div class="preview-icon bg-warning">
-                  <i class="ti-settings mx-0"></i>
-                </div>
-              </div>
-              <div class="preview-item-content">
-                <h6 class="preview-subject font-weight-normal">Settings</h6>
-                <p class="font-weight-light small-text mb-0 text-muted">
-                  Private message
-                </p>
-              </div>
-            </a>
-            <a class="dropdown-item preview-item">
-              <div class="preview-thumbnail">
-                <div class="preview-icon bg-info">
-                  <i class="ti-user mx-0"></i>
-                </div>
-              </div>
-              <div class="preview-item-content">
-                <h6 class="preview-subject font-weight-normal">New user registration</h6>
-                <p class="font-weight-light small-text mb-0 text-muted">
-                  2 days ago
-                </p>
-              </div>
-            </a>
-          </div>
-        </li>
-        <li class="nav-item nav-profile dropdown">
-          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-            <img src="images/faces/face28.jpg" alt="profile"/>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-            <a class="dropdown-item">
-              <i class="ti-settings text-primary"></i>
-              Settings
-            </a>
-            <a class="dropdown-item">
-              <i class="ti-power-off text-primary"></i>
-              Logout
-            </a>
-          </div>
-        </li>
-        <li class="nav-item nav-settings d-none d-lg-flex">
-          <a class="nav-link" href="#">
-            <i class="icon-ellipsis"></i>
-          </a>
-        </li>
-      </ul>
-      <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-        <span class="icon-menu"></span>
-      </button>
-    </div>
-  </nav>
+        </div>
+      </nav>
+  </div>
 
   <div class="container">
     @yield('page-heading')
