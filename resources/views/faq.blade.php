ce6ecb313269ff8082ec85185b7d9b457c9a7e02
I Putu Martin Winata <94847832+winataa@users.noreply.github.com>
2023-01-01 09:55:59 +0800

    Add files via upload

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <style>
+      img{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    </style>
+    <title>FAQ Section</title>
+  </head>
+  <body>
+    <div class="text-center">
+      <h1 class="mt-5 mb-5"><strong>Frequently Asked Questions (FAQ)</strong></h1>
+    </div>
+    <img src="faq.png" alt="FAQ Illustration" style="width:450px;width:450px;">
+    <section class="container my-5" id="maincontent">
+      <section id="accordion">
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary border-top" aria-controls="faq-17" aria-expanded="false" data-toggle="collapse" href="#faq-17" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              How's our background?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-17" style="">
+          <div class="card card-body border-0 p-0">
+            <p><strong>Tadika Laundry</strong> is a part of IT module integrated with the hotel main system 
+              <strong> Tadika Mesra</strong> which established since October 2022.
+            </p>
+  
+          </div>
+        </div>
+  
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary border-top" aria-controls="faq-18" aria-expanded="false" data-toggle="collapse" href="#faq-18" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              What is our main goal?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-18" style="">
+          <div class="card card-body border-0 p-0">
+            <p>Our official mission is to provide an end to end cleaning for every guests' sheets.</p>
+            <p>
+            </p>
+          </div>
+        </div>
+  
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary border-top" aria-controls="faq-19" aria-expanded="false" data-toggle="collapse" href="#faq-19" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              What services we present to you?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-19" style="">
+          <div class="card card-body border-0 p-0">
+            <p>In order to reach our purpose, we offer you various services such as laundry etc.</p>
+            <p>
+            </p>
+          </div>
+        </div>
+  
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary  border-top" aria-controls="faq-20" aria-expanded="false" data-toggle="collapse" href="#faq-20" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              What is the best email to reach you at?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-20">
+          <div class="card card-body border-0 p-0">
+            <p>The best email for any inquiries is laundry@tadika.com!</p>
+            <p>
+            </p>
+          </div>
+        </div>
+  
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary  border-top" aria-controls="faq-21" aria-expanded="false" data-toggle="collapse" href="#faq-21" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              Where can I read more about this company?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-21">
+          <div class="card card-body border-0 p-0">
+            <p>Lorem ipsum dolor sit!</p>
+            <p>
+            </p>
+          </div>
+        </div>
+  
+        <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary  border-top" aria-controls="faq-22" aria-expanded="false" data-toggle="collapse" href="#faq-22" role="button">
+          <div class="position-relative">
+            <h2 class="h4 m-0 pr-3">
+              What is the best time to call?
+            </h2>
+            <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
+              <i class="fa fa-plus"></i>
+            </div>
+          </div>
+        </a>
+        <div class="collapse" id="faq-22">
+          <div class="card card-body border-0 p-0">
+            <p>The best time to call is 24/7! We are always available to answer any questions.</p>
+            <p>
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+  </body>
+</html>
